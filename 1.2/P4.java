2c0b31f7034bc533f702deb30597163ca93ccb3e
mandouss <zsy.amanda@gmail.com>
2019-07-24 18:21:38 +0800

    P4 & P5

---
+++ 1.2/P4.java
@@ -0,0 +1,10 @@
+
+public class P4 {
+    public static void main(String args[]) {
+        String str1 = "hello";
+        String str2 = str1;
+        str1 = "world";
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
---
+++ 1.2/P5.java
@@ -0,0 +1,9 @@
+
+public class P5 {
+    public static void main(String args[]) {
+        String str1 = "hello world";
+        str1.toUpperCase();
+        str1.substring(6, 11);
+        System.out.println(str1);
+    }
+}
