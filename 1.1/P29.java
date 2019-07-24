64ac0398a17ab834b9e8b1967eb7e375f01ee2e4
mandouss <zsy.amanda@gmail.com>
2019-07-24 10:13:06 +0800

    29 revised

--- 1.1/P29.java
+++ 1.1/P29.java
@@ -7,43 +7,36 @@
  */
 
 public class P29 {
-    /* count for the frequency of the keys*/
     
-    public static int count(int a, int b) {
-        return b - a + 1;
-    }
 
-    public static int countFront(int key, int[] a) {
-        int count = 0;
-        int mid = rank(key, a);
-        for(int key1 = mid - 1; key1 >= 0; key1--) {
-            if (a[key1] == a[key1 + 1]) {
-                count++;
-            } else {
-                break;
-            }
+    public static int countFront(int mid, int[] a) {
+        int mid1 = mid - 1;
+        while(mid1 >= 0 && a[mid1] == a[mid1 + 1]) { //guarantee the index not to overflow
+            mid1--;
         }
-        return key1 + 1;
+        return mid1 + 1;
     }
 
-    public static int countAfter(int key, int[] a) {
-        int count = 0;
-        int mid = rank(key, a);
-        for(int key2 = key + 1; key2 < a.length; key2++) {
-            if (a[key2] == a[key2 - 1]) {
+    /* count for the frequency of the keys*/
+    public static int count(int midFirst, int[] a) {
+        if (midFirst == -1) {
+            return 0;
+        } else {
+            int count = 1;
+            int mid2 = midFirst;
+            while(mid2 < a.length - 1 && a[mid2] == a[mid2 + 1]) { //guarantee the index not to overflow
                 count++;
-            } else {
-                break;
+                mid2++;
             }
+            return count;
         }
-        return key2 - 1;
     }
 
-
-
+    //find the index of the key
     public static int rank(int key, int[] a) {
         int lo = 0;
         int hi = a.length - 1;
+        int firstKey = - 1;
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
             if (key < a[mid]) {
@@ -51,18 +44,20 @@ public class P29 {
             } else if (key > a[mid]) {
                 lo = mid  + 1;
             } else {
-                int firstKey = countFront(mid, a);
-                int LastKey = countFront(mid, a);
-                return LastKey - firstKey + 1;
+                //return the number of the elements that are smaller than the key
+                firstKey = countFront(mid, a); 
+                return firstKey;
             }
         }
         return -1;
     }
 
     public static void main(String[] args) {
-        int[] array = {0,1,2,3,4,5,6,7,8,9,10,10,10,10,12,13,14,15,16};
+        int[] array = {0,0,0,0,0,1,2,3,4,5,6,7,8,9,10,10,10,10};
         int key = 10;
-        int findRes = rank(key, array);
-        System.out.println(findRes);
+        int findFirst = rank(key, array);
+        System.out.println(findFirst + "\n");
+        int findAll = count(findFirst, array);
+        System.out.print(findAll);
     }
 }
